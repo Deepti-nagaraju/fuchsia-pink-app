1398358095e9206107d152ebb3cf7924bd03e7df
LENOVO <LENOVO@DESKTOP-MLE9BR1>
2025-07-11 11:46:09 +0530

    fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,6 +15,7 @@ export const routes: Routes = [
         path: 'dashboard',
         loadComponent: () => import('../app/shop/pages/shop/shop.component').then(m => m.ShopComponent)
     },
+    // cart 
       {
     path: 'cart',
     loadComponent: () => import('../app/shop/pages/cart/cart.component').then(m => m.CartComponent)
--- src/app/shop/pages/cart/cart.component.ts
+++ src/app/shop/pages/cart/cart.component.ts
@@ -3,7 +3,7 @@ import { CartService } from '../../../shared/services/cart.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-// ✅ Add the following components
+
 import { HeaderOneComponent } from '../../../shared/header/header-one/header-one.component';
 import { FooterOneComponent } from '../../../shared/footer/footer-one/footer-one.component';
 import { BreadcrumbOneComponent } from '../../../shared/components/breadcrumb/breadcrumb-one/breadcrumb-one.component';
